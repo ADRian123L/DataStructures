2049be68331c54e486cea38b3686fa1310eae492
ADRian123L <adrian163@usf.edu>
2023-09-05 16:45:26 -0400

    Completed the class. I need to fix the string meth

--- ProgProjects/PP1(CalcList)/CalcList.cpp
+++ ProgProjects/PP1(CalcList)/CalcList.cpp
@@ -2,6 +2,8 @@
 #include <iostream>
 #include <string>
 
+std::string oper(FUNCTIONS func);
+
 class CalcList : public CalcListInterface {
 private:
     // Class for the linked list:
@@ -13,23 +15,29 @@ private:
         Node     *next;
         Node(double Rightval, double Leftval, Node *ptr)
             : rightOperand(Rightval), leftOperand(Leftval), next(ptr) {}
-        Node(double Rightval, double Leftval)
-            : rightOperand(Rightval), leftOperand(Leftval) {
-            next = nullptr;
-        }
     };
 
     // Declaring and initializing the main variables:
     double totalSum = 0;
     Node  *head     = nullptr;
+    size_t numNode  = 0;
 
 public:
-    double      total() const;
-    void        newOperation(const FUNCTIONS func, const double operand);
-    void        removeLastOperation();
-    std::string toString(unsigned short precision) const;
+    double total() const override;
+    void   newOperation(const FUNCTIONS func, const double operand) override;
+    void   removeLastOperation() override;
+    std::string toString(unsigned short precision) const override;
     ~CalcList();
 };
+
+CalcList::~CalcList() {
+    Node *ptr = head;
+    while (head != nullptr) {
+        head = head->next;
+        delete ptr;
+        ptr = head;
+    }
+}
 
 double CalcList::total() const { return totalSum; }
 
@@ -37,6 +45,7 @@ void CalcList::newOperation(const FUNCTIONS func, const double operand) {
     try {
         Node *node = new Node(this->totalSum, operand, head);
         head       = node;
+        ++(this->numNode);
     } catch (const std::bad_alloc &e) {
         std::cout << "Memory allocation failed: " << e.what() << std::endl;
     }
@@ -61,9 +70,41 @@ void CalcList::newOperation(const FUNCTIONS func, const double operand) {
     }
 }
 
-void CalcList::removeLastOperation() {}
+void CalcList::removeLastOperation() {
+    if (head != nullptr) {
+        this->totalSum = head->rightOperand;
+        Node *ptr      = head;
+        head           = head->next;
+        delete ptr;
+        --(this->numNode);
+    }
+}
 
 std::string CalcList::toString(unsigned short precision) const {
-    std::string ans;
+    std::string ans((20 + precision * 3), '\0');
+    Node       *ptr = this->head;
+    for (size_t i = numNode; i > 0; --i, ptr = ptr->next) {
+        ans += std::to_string(i) + std::string(": ");
+        ans += std::string(std::to_string(static_cast<int>(ptr->rightOperand)));
+        ans += oper(ptr->funct) + std::string("+");
+        ans += std::to_string(3);
+    }
     return ans;
+}
+
+std::string oper(FUNCTIONS func) {
+    switch (func) {
+    case ADDITION: {
+        return std::string("+");
+    }
+    case SUBTRACTION: {
+        return std::string("-");
+    }
+    case MULTIPLICATION: {
+        return std::string("*");
+    }
+    case DIVISION: {
+        return std::string("/");
+    }
+    };
 }
