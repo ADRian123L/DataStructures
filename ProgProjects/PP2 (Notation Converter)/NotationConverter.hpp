5cb3115bcc8aed8dbfe88882e6e4e2753e71cf15
ADRian123L <adrian163@usf.edu>
2023-10-02 23:23:41 -0400

    Edited

--- ProgProjects/PP2 (Notation Converter)/NotationConverter.hpp
+++ ProgProjects/PP2 (Notation Converter)/NotationConverter.hpp
@@ -18,185 +18,146 @@ private:
     template <typename T>
     class Deque {
     private:
-        // 'DoubleList' implements the circular doubly linked list:
         class DoubleList {
         private:
-            // The node for the circular double linked list:
             class DoubleNode {
             public:
-                // Pointers:
                 DoubleNode *next;
                 DoubleNode *prev;
-                // Value
-                T val;
-                // Constructors:
+                T           val;
                 DoubleNode(const T    &val,
-                           DoubleNode *next_ptr,
-                           DoubleNode *prev_ptr)
+                           DoubleNode *next_ptr = nullptr,
+                           DoubleNode *prev_ptr = nullptr)
                     : val(val), next(next_ptr), prev(prev_ptr) {}
-                DoubleNode(const T &val) : val(val) {}
-                DoubleNode(DoubleNode *next, DoubleNode *prev)
-                    : next(next), prev(prev) {}
             };
 
             class Iterator {
             public:
-                friend class DoubleNode;
-
-            private:
+                // TODO: Implement Iterator functionality.
             };
 
             std::size_t number_of_nodes;
-            // Sentinel Node:
-            DoubleNode *sentinel = new DoubleNode;
+            DoubleNode *sentinel;
 
         public:
-            DoubleList() {
-                // Create an object of DoubleNode:
-                // this->sentinel = new DoubleNode;
-                // Point next and prev to the sentinel node:
-                this->sentinel->next = this->sentinel;
-                this->sentinel->prev = this->sentinel;
-                // Set the number of nodes to zero:
-                this->number_of_nodes = 0;
+            DoubleList() : number_of_nodes(0) {
+                sentinel =
+                    new DoubleNode(T()); // Initialized with default value.
+                sentinel->next = sentinel;
+                sentinel->prev = sentinel;
             }
 
-            // Deallocates all of the nodes' memory:
             ~DoubleList() {
-                // Declare and initialize temp nodes:
-                DoubleNode *ptr_current   = this->sentinel->next;
-                DoubleNode *ptr_to_delete = nullptr;
-                // Delete all of the nodes:
-                while (ptr_current != sentinel) {
-                    ptr_to_delete = ptr_current;
-                    ptr_current   = ptr_current->next;
-                    delete ptr_to_delete;
+                DoubleNode *current = sentinel->next;
+                while (current != sentinel) {
+                    DoubleNode *nextNode = current->next;
+                    delete current;
+                    current = nextNode;
                 }
-                // Delete the sentinel node:
-                delete this->sentinel;
-                this->sentinel = nullptr;
+                delete sentinel;
             }
 
-            // Queries:
-            bool        isEmpty() const { return this->number_of_nodes == 0; }
-            std::size_t size() const { return this->number_of_nodes; }
+            bool        isEmpty() const { return number_of_nodes == 0; }
+            std::size_t size() const { return number_of_nodes; }
             const T    &getNode(std::size_t index) const {
-                DoubleNode *ptr = this->sentinel->next;
-                for (std::size_t i = 0; i < index; ++i, ptr = ptr->next)
-                    ;
+                if (index >= size()) {
+                    throw std::out_of_range("Index out of range");
+                }
+                DoubleNode *ptr = sentinel->next;
+                for (std::size_t i = 0; i < index; ++i) {
+                    ptr = ptr->next;
+                }
                 return ptr->val;
             }
 
-            Iterator begin() const;
-            Iterator end() const;
+            // TODO: Implement the begin() and end() iterator methods.
 
-            // Main operations:
             void pushBack(const T &value) {
-                // create a new node and point 'next' and 'prev' to the
-                // corresponding nodes:
                 DoubleNode *new_node =
-                    new DoubleNode(value, this->sentinel, this->sentinel->prev);
-                this->sentinel->prev = new_node;
-                new_node->prev->next = new_node;
-                this->number_of_nodes++;
+                    new DoubleNode(value, sentinel, sentinel->prev);
+                sentinel->prev->next = new_node;
+                sentinel->prev       = new_node;
+                number_of_nodes++;
             }
+
             void pushFront(const T &value) {
-                // create a new node and point 'next' and 'prev' to the
-                // corresponding nodes:
                 DoubleNode *new_node =
-                    new DoubleNode(value, this->sentinel->next, this->sentinel);
-                this->sentinel->next = new_node;
-                new_node->next->prev = new_node;
-                this->number_of_nodes++;
+                    new DoubleNode(value, sentinel->next, sentinel);
+                sentinel->next->prev = new_node;
+                sentinel->next       = new_node;
+                number_of_nodes++;
             }
+
             void popBack() {
-                // Check if the list is empty:
-                if (isEmpty()) {
-                    // If empty throw an exception:
-                    throw std::runtime_error("The list is empty");
-                }
-                else {
-                    // Delete the node by creating a temp node moving the
-                    // pointers:
-                    DoubleNode *temp_node = this->sentinel->prev;
-                    this->sentinel->prev  = temp_node->prev;
-                    temp_node->prev->next = this->sentinel;
-                    delete temp_node;
-                    this->number_of_nodes--;
-                }
+                if (isEmpty()) throw std::runtime_error("The list is empty");
+                DoubleNode *toDelete = sentinel->prev;
+                sentinel->prev       = toDelete->prev;
+                toDelete->prev->next = sentinel;
+                delete toDelete;
+                number_of_nodes--;
             }
+
             void popFront() {
-                // Check if the list is empty:
-                if (isEmpty()) {
-                    // If empty throw an exception:
-                    throw std::runtime_error("The list is empty");
-                }
-                else {
-                    DoubleNode *temp_node = this->sentinel->next;
-                    this->sentinel->next  = temp_node->next;
-                    temp_node->next->prev = this->sentinel;
-                    delete temp_node;
-                    this->number_of_nodes--;
-                }
+                if (isEmpty()) throw std::runtime_error("The list is empty");
+                DoubleNode *toDelete = sentinel->next;
+                sentinel->next       = toDelete->next;
+                toDelete->next->prev = sentinel;
+                delete toDelete;
+                number_of_nodes--;
             }
 
             void insert(std::size_t index, const T &value) {
-                if (index <= size()) {
-                    DoubleNode *ptr = this->sentinel->next;
-                    // Search for the node:
-                    for (std::size_t i = 0; i < index; ++i, ptr = ptr->next)
-                        ;
-                    // Create a new node:
-                    DoubleNode *new_node =
-                        new DoubleNode(value, ptr->next, ptr);
-                    // Change where the nodes point to:
-                    ptr->next            = new_node;
-                    new_node->next->prev = new_node;
-                    new_node->prev->next = new_node;
-                }
-                else {
+                if (index > size()) {
                     throw std::out_of_range("The index is out of range");
                 }
+                if (index == 0) {
+                    pushFront(value);
+                }
+                else if (index == size()) {
+                    pushBack(value);
+                }
+                else {
+                    DoubleNode *ptr = sentinel->next;
+                    for (std::size_t i = 0; i < index; ++i) {
+                        ptr = ptr->next;
+                    }
+                    DoubleNode *new_node =
+                        new DoubleNode(value, ptr, ptr->prev);
+                    ptr->prev->next = new_node;
+                    ptr->prev       = new_node;
+                    number_of_nodes++;
+                }
             }
+
             void setVal(std::size_t index, T value) {
-                if (index < size()) {
-                    DoubleNode *ptr = this->sentinel->next;
-                    // Search for the node:
-                    for (std::size_t i = 0; i < index; ++i, ptr = ptr->next)
-                        ;
-                    ptr->val = value;
+                if (index >= size()) {
+                    throw std::out_of_range("Index out of range");
                 }
-                else {
-                    throw std::runtime_error(
-                        "The list is either empty or the index is out of "
-                        "range");
+                DoubleNode *ptr = sentinel->next;
+                for (std::size_t i = 0; i < index; ++i) {
+                    ptr = ptr->next;
                 }
+                ptr->val = value;
             }
-            void insert(Iterator &position, T value) {
-                if (!isEmpty()) {
-                }
-                else {
-                    throw std::runtime_error("The list is empty");
-                }
-            }
-            void setVal(Iterator &position, T value);
+
+            // TODO: Implement the methods using the Iterator.
         };
-        DoubleList *double_list = nullptr;
+
+        DoubleList *double_list;
 
     public:
-        Deque() { this->double_list = new DoubleList; }
-        ~Deque() { delete this->double_list; }
-        // Queries:
-        std::size_t size(void) const { return this->double_list->size(); }
-        bool        isEmpty(void) const { return this->double_list->isEmpty(); }
-        const T    &front() const { return this->double_list->getNode(0); }
-        const T &back() const { return this->double_list->getNode(size() - 1); }
+        Deque() : double_list(new DoubleList()) {}
+        ~Deque() { delete double_list; }
 
-        // Operations:
-        void insertFront(T &value) { this->double_list->pushFront(value); }
-        void insertBack(T &value) { this->double_list->pushBack(value); }
-        void eraseFront() { this->double_list->popFront(); }
-        void eraseBack() { this->double_list->popBack(); }
+        std::size_t size() const { return double_list->size(); }
+        bool        isEmpty() const { return double_list->isEmpty(); }
+        const T    &front() const { return double_list->getNode(0); }
+        const T    &back() const { return double_list->getNode(size() - 1); }
+
+        void insertFront(const T &value) { double_list->pushFront(value); }
+        void insertBack(const T &value) { double_list->pushBack(value); }
+        void eraseFront() { double_list->popFront(); }
+        void eraseBack() { double_list->popBack(); }
     };
 };
 
