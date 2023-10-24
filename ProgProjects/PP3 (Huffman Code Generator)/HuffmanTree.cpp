7e1b07f3957960d2cf42dd4243c36286fe74d294
ADRian123L <adrian163@usf.edu>
2023-10-23 21:01:58 -0400

    Add the first functions

--- ProgProjects/PP3 (Huffman Code Generator)/HuffmanTree.cpp
+++ ProgProjects/PP3 (Huffman Code Generator)/HuffmanTree.cpp
@@ -1,16 +1,30 @@
+#include "HuffmanTree.hpp"
 
-#ifndef HUFFMANTREE_H
-#define HUFFMANTREE_H
+HuffmanTree::~HuffmanTree() {}
 
-#include "HuffmanBase.hpp"
+std::string HuffmanTree::serializeTree() const {
+    std::string answer;
+    return answer;
+}
 
-class HuffmanTree : public HuffmanNode {
-public:
-    std::string compress(const std::string inputStr);
-    std::string serializeTree() const;
-    std::string decompress(const std::string inputCode,
-                           const std::string serializedTree);
+std::string HuffmanTree::decompress(const std::string inputCode,
+                                    const std::string serializedTree) {
+    std::string answer;
+    return answer;
+}
 
-private:
-};
-#endif /* HUFFMANTREE_H */
+std::string HuffmanTree::compress(const std::string inputStr) {
+    std::string compressed_hoff_code;
+    int         char_frequencies[128]{0}; // Hash map
+    // Count the frequency of each letter:
+    for (auto &ptr : inputStr) {
+        ++char_frequencies[static_cast<int>(ptr)];
+    }
+    return compressed_hoff_code;
+}
+
+int main() {
+    HuffmanTree t;
+    t.compress("Hello There Are you?AA");
+    return EXIT_SUCCESS;
+}
--- ProgProjects/PP3 (Huffman Code Generator)/HuffmanTree.hpp
+++ ProgProjects/PP3 (Huffman Code Generator)/HuffmanTree.hpp
@@ -2,6 +2,20 @@
 #define HUFFMANTREE_H
 
 #include "HuffmanBase.hpp"
+#include <string>
 
-class HuffmanTree : friend HuffmanBase {};
-#endif
+class HuffmanTree : public HuffmanTreeBase {
+public:
+    // Constructor // Destructor
+    HuffmanTree() : ptr_to_tree(nullptr) {}
+    ~HuffmanTree();
+
+    std::string compress(const std::string inputStr) override;
+    std::string serializeTree() const override;
+    std::string decompress(const std::string inputCode,
+                           const std::string serializedTree) override;
+
+private:
+    HuffmanNode *ptr_to_tree; // It points to the tree
+};
+#endif /* HUFFMANTREE_H */
