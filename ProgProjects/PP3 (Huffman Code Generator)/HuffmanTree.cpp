f4f3ffb15633d0fa45b3d8daf11ce2ae910ccb26
ADRian123L <adrian163@usf.edu>
2023-10-25 00:41:25 -0400

    Completed the first function for the Huff code

--- ProgProjects/PP3 (Huffman Code Generator)/HuffmanTree.cpp
+++ ProgProjects/PP3 (Huffman Code Generator)/HuffmanTree.cpp
@@ -15,12 +15,57 @@ std::string HuffmanTree::decompress(const std::string inputCode,
 
 std::string HuffmanTree::compress(const std::string inputStr) {
     std::string compressed_hoff_code;
-    int         char_frequencies[128]{0}; // Hash map
+    size_t      char_frequencies[128]{0}; // Hash map
     // Count the frequency of each letter:
     for (auto &ptr : inputStr) {
         ++char_frequencies[static_cast<int>(ptr)];
     }
+    // Create each node and insert them into queue:
+    HeapQueue<HuffmanNode *, HuffmanNode::Compare> que;
+    HuffmanNode                                   *ptr_to_each_node = nullptr;
+    for (std::size_t i = 0; i < 127; i++) {
+        if (char_frequencies[i]) {
+            ptr_to_each_node = new HuffmanNode(char(i), char_frequencies[i]);
+            que.insert(ptr_to_each_node);
+        }
+    }
+    // Build the tree:
+    while (que.size() > 1) {
+        HuffmanNode *tmp_1 = nullptr, *tmp_2 = nullptr;
+        tmp_1 = que.min();
+        que.removeMin();
+        tmp_2 = que.min();
+        que.removeMin();
+        ptr_to_each_node =
+            new HuffmanNode(char(),
+                            tmp_1->getFrequency() + tmp_2->getFrequency(),
+                            ptr_to_each_node,
+                            tmp_1,
+                            tmp_2);
+        que.insert(ptr_to_each_node);
+    }
+    ptr_to_tree = que.min();
+    que.removeMin();
+    // Create the Huffman code:
+    std::cout << HuffTable(*ptr_to_tree);
     return compressed_hoff_code;
+}
+
+std::string HuffmanTree::HuffTable(HuffmanNode const &node) const {
+    static std::string answer;
+    if (node.isLeaf()) {
+        answer.push_back(node.getCharacter());
+        return answer;
+    }
+    if (node.left != nullptr) {
+        answer.push_back('L');
+        HuffTable(*node.left);
+    }
+    if (node.right != nullptr) {
+        answer.push_back('R');
+        HuffTable(*node.right);
+    }
+    return answer;
 }
 
 int main() {
--- ProgProjects/PP3 (Huffman Code Generator)/HuffmanTree.hpp
+++ ProgProjects/PP3 (Huffman Code Generator)/HuffmanTree.hpp
@@ -1,6 +1,7 @@
 #ifndef HUFFMANTREE_H
 #define HUFFMANTREE_H
 
+#include "HeapQueue.hpp"
 #include "HuffmanBase.hpp"
 #include <string>
 
@@ -14,6 +15,9 @@ public:
     std::string serializeTree() const override;
     std::string decompress(const std::string inputCode,
                            const std::string serializedTree) override;
+
+    // Helper functions:
+    std::string HuffTable(HuffmanNode const &node) const;
 
 private:
     HuffmanNode *ptr_to_tree; // It points to the tree
